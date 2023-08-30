b380bbad81303e0b43c9fefe2a5b6115517f06f3
Ayvaz Izmailov <saltinthecut@gmail.com>
2023-08-30 11:13:30 +0300

    added pagination

--- core/views.py
+++ core/views.py
@@ -3,6 +3,13 @@ from .serializers import PostSerializer
 from .models import Post
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import pagination
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -10,3 +17,4 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
